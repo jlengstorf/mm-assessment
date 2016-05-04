de97f7ee37bd5a6342fc0dbfa610f9de6097657b
jlengstorf <jason.lengstorf@copterlabs.com>
2016-05-04 22:32:19 +0900

    feat(env): added env vars to avoid dev builds of React/Redux

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV || 'development'}"`,
+    }),
   ],
   module: {
     loaders: [
