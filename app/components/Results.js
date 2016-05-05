992a0a577ac3de4ca2f7f0be100664b96d3e5990
jlengstorf <jason.lengstorf@copterlabs.com>
2016-05-05 13:37:21 +0900

    feat(loading): trying to catch the loading bug

--- app/components/Results.js
+++ app/components/Results.js
@@ -15,15 +15,12 @@ const Results = ({ isVisible, messages, currentResults = 0, onMessagesEmpty }) =
     loading = true;
   }
 
-  if (loading === true) {
-    return (<div className="mm-assessment__loading">Loading results...</div>);
-  }
-
   const image = messageData.image && messageData.image.pop();
   const email = messageData.email_message && messageData.email_message[0];
 
   const classes = ['mm-assessment__results'];
   !isVisible && classes.push('mm-assessment__results--hidden');
+  loading && classes.push('mm-assessment__results--loading');
 
   return (
     <section className={classes.join(' ')}>
